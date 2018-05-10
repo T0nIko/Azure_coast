4e9ee507540d4f8cb5df8c7277c7fd4451294e60
Dasha <etterwin.yll@gmail.com>
2018-05-10 15:49:18 +0300

    update

---
+++ js/main.js
@@ -0,0 +1,22 @@
+$(document).ready(function () {
+    $('.marker__container').on('mouseout', function () {
+        $('div.marker__pop-up').removeClass('marker__pop-up--active')
+    });
+
+    $('.marker-wrapper').on('mouseover', function () {
+        $(this).siblings('div.marker__pop-up').addClass('marker__pop-up--active');
+    });
+
+    $('.map-btn').on('click', function () {
+        $('.map-btn').removeClass('active');
+        $(this).addClass('active');
+    });
+
+    $(document).mouseup(function (e) {
+        let button = $('.map-btn');
+        if (!button.is(e.target) && button.has(e.target).length === 0) {
+            $(button).removeClass('active');
+        }
+    });
+
+});
