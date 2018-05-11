eea8ed9f04eca197a422b91a0526f574d7f74694
Dasha <etterwin.yll@gmail.com>
2018-05-11 13:10:18 +0300

    update

--- js/main.js
+++ js/main.js
@@ -1,27 +1,68 @@
 $(document).ready(function () {
-    $('.marker__container').on('mouseout', function () {
-        $('div.marker__pop-up').removeClass('marker__pop-up--active')
+    // Hide info pop-up if it hasn't target
+    $('.marker-wrapper').on('mouseout', function () {
+        $('div.marker__pop-up.marker__pop-up--active').removeClass('marker__pop-up--active');
+        setTimeout(function () {
+            $('div.marker__pop-up.marker__pop-up--main').removeClass('marker__pop-up--main');
+        },100);
     });
 
-    $('.marker-wrapper').on('mouseover', function () {
-        $(this).siblings('div.marker__pop-up').addClass('marker__pop-up--active');
+    // Show info pop-up
+    $('.marker__container').on('mouseover', function () {
+        let main = $(this).find('div.marker__pop-up');
+        $(main).addClass('marker__pop-up--main');
+        setTimeout(function () {
+            $(main).addClass('marker__pop-up--active');
+        },100);
     });
 
     $('.map-btn').on('click', function () {
         $('.map-btn').removeClass('active');
         $(this).addClass('active');
+
+        // For sale
+        if ($('.map-btn.active').hasClass('map-btn--darkblue')) {
+            $('.field').removeClass('field--active');
+            $('.field.field--sale').addClass('field--active');
+        }
+        // For sold
+        else if ($('.map-btn.active').hasClass('map-btn--orange')) {
+            $('.field').removeClass('field--active');
+            $('.field.field--sold').addClass('field--active');
+        }
+        // For share
+        else {
+            $('.field').removeClass('field--active');
+            $('.field.field--share').addClass('field--active');
+        }
     });
 
+    // Remove button style if it hasn't target
     $(document).mouseup(function (e) {
         let button = $('.map-btn');
         if (!button.is(e.target) && button.has(e.target).length === 0) {
             $(button).removeClass('active');
+            $('.field').removeClass('field--active');
         }
     });
 
+    // Show field's info
     $('.field').on('mouseover', function (e) {
-        e.preventDefault();
-        $(this).find('.info-wrapper').addClass('info-wrapper--active');
+        let main = $(this).find('.info-wrapper');
+        $(main).addClass('info-wrapper--main');
+        setTimeout(function () {
+            $(main).addClass('info-wrapper--active');
+        },100);
+    });
+
+    // Hide field's info if it hasn't target
+    $('section.field').on('mouseout', function (e) {
+        $('.info-wrapper').removeClass('info-wrapper--active', 'info-wrapper--main');
+        let main = $(this).find('.info-wrapper');
+        $(main).removeClass('info-wrapper--active');
+        setTimeout(function () {
+            $(main).removeClass('info-wrapper--main');
+        },100);
     });
 
 });
